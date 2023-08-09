aa9ab46dc6622a7b8bdf376e199f83b5bba2d0f5
Linda Givers <lindagivers@gmail.com>
2023-08-09 15:28:42 -0400

    final push

--- SignUpForm.jsx
+++ SignUpForm.jsx
@@ -8,20 +8,39 @@ export default function SignUpForm() {
 
     async function handleSubmit(event) {
         event.preventDefault();
-        console.log("Hello");
-      }
+        //console.log("Hello");
+
+        try{
+            const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup",{
+                method:"POST",
+                body:JSON.stringify ({username},{password})
+            })
+            const result = await response.json();
+            console.log(result);
+
+        } catch (error){
+            setError(error.message);
+        }
+        setToken(result.token);
+    }
 
     return (
     <>
     <h2>Sign Up!</h2>
-
+    {error && <p>{error}</p>}
     <form onSubmit={handleSubmit}>
         <label>
-            Username: <input value={username} onChange={(e) => setUserName(e.target.value)}/>
+            Username: {" "}
+            <input type="username"
+            value={username} 
+            onChange={(e) => setUserName(e.target.value)}/>
         </label>
         <br/>
         <label>
-            Password: <input value={password} onChange={(e) => setPassword(e.target.value)}/>
+            Password: {" "}
+            <input type="password"
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)}/>
         </label>
         <br/>
         <button>Submit</button>
